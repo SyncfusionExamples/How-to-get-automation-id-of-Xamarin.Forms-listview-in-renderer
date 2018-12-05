1c0cf43a9c0f66f0e3db8b6d02eab504a98c9146
Gnana Priya <gnanapriya.namasivayam@syncfusion.com>
2018-12-05 15:47:54 +0530

    Added sample to get a automation id in renderer.

---
+++ GetAutomationIdInListView/GetAutomationIdInListView/GetAutomationIdInListView.iOS/SfListViewRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using SfListViewSample;
+using SfListViewSample.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(SfListViewRenderer))]
+
+namespace SfListViewSample.iOS
+{
+   public class SfListViewRenderer: VisualElementRenderer<ExtendedListView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ExtendedListView> e)
+        {
+            base.OnElementChanged(e);
+            var id = this.Element.AutomationId;
+            System.Diagnostics.Debug.WriteLine("iOS Renderer Automation ID" + id);
+
+        }
+    }
+}
